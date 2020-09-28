259163c3c049b9384fcb7539da68a0ff36a9ef9d
Anastasiya Zavrazhneva <zavrazhneva.a@gmail.com>
2020-09-28 20:00:35 +0300

    feat: add min, max, avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(!array || array.length === 0) return 0
+    return array.reduce((acc,item) => {
+        return Math.min(acc, item);
+    }, 0)
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(!array || array.length === 0) return 0
+  return array.reduce((acc,item) => {
+        return Math.max(acc, item);
+    }, 0)
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(!array || array.length === 0) return 0
+    return  array.reduce((acc,item) => {
+        return acc + item;
+    }, 0) / array.length
 }
